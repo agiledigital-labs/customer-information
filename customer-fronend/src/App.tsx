8f755f7a0ce0c659d7ad9465cc7f7d105408ec5d
Sam Murphy <stmurphy@agiledigital.com.au>
2021-05-07 15:38:52 +1000

    Add google auth

--- customer-fronend/src/App.tsx
+++ customer-fronend/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Auth from "@aws-amplify/auth";
-import { AuthProvider, Resource } from "ra-core";
-import { Admin, fetchUtils } from "react-admin";
-import postgrestRestProvider from "@raphiniert/ra-data-postgrest";
-import { UsersCreate, UsersEdit, UsersList } from "./UsersList";
-import AmplifyReduxAuth, { authSagas, authState } from "amplify-redux-auth";
-import axios from "axios";
+import React from 'react';
+import Auth from '@aws-amplify/auth';
+import { AuthProvider, Resource } from 'ra-core';
+import { Admin, fetchUtils } from 'react-admin';
+import postgrestRestProvider from '@raphiniert/ra-data-postgrest';
+import { UsersCreate, UsersEdit, UsersList } from './UsersList';
+import AmplifyReduxAuth, { authSagas, authState } from 'amplify-redux-auth';
+import axios from 'axios';
 
 const authProvider: AuthProvider = {
   login: async () => undefined,
@@ -15,24 +15,33 @@ const authProvider: AuthProvider = {
   getPermissions: async () => Auth.currentAuthenticatedUser(),
 };
 
+const CustomLoginPage = () => {
+  Auth.federatedSignIn();
+
+  return <></>;
+};
+
+
 const LoginPage = () => (
-  <AmplifyReduxAuth logoText={"My Logo"}>Logged in!</AmplifyReduxAuth>
+  <AmplifyReduxAuth logoText={'My Logo'} AuthComponent={<CustomLoginPage />}>
+    Logged in!
+  </AmplifyReduxAuth>
 );
 
 const httpClient = async (url: string, options = {}) => {
   // @ts-ignore
   if (!options.headers) {
     // @ts-ignore
-    options.headers = new Headers({ Accept: "application/json" });
+    options.headers = new Headers({ Accept: 'application/json' });
   }
   const test = await Auth.currentAuthenticatedUser();
   const token = await test.getSignInUserSession().getIdToken().getJwtToken();
   console.log(token);
   // add your own headers here
   // @ts-ignore
-  options.headers.set("X-Custom-Header", "foobar");
+  options.headers.set('X-Custom-Header', 'foobar');
   // @ts-ignore
-  options.headers.set("Authorization", `Bearer ${token}`);
+  options.headers.set('Authorization', `Bearer ${token}`);
   return fetchUtils.fetchJson(url, options);
 };
 
@@ -44,7 +53,7 @@ const App = () => {
         customReducers={{ authState }}
         customSagas={[authSagas]}
         dataProvider={postgrestRestProvider(
-          "https://poqhfgyide.execute-api.ap-southeast-2.amazonaws.com",
+          'https://poqhfgyide.execute-api.ap-southeast-2.amazonaws.com',
           httpClient
         )}
         authProvider={authProvider}
--- customer-fronend/src/index.tsx
+++ customer-fronend/src/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { cleanEnv, str } from "envalid";
-import AmplifyReduxAuth, { configureAmplify } from "amplify-redux-auth";
-import { Provider } from "react-redux";
-import { createAdminStore } from "ra-core";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { cleanEnv, str } from 'envalid';
+import { configureAmplify } from 'amplify-redux-auth';
+import { Auth } from '@aws-amplify/auth';
 
 const parsedEnv = cleanEnv(process.env, {
-  REACT_APP_AWS_REGION: str({ default: "ap-southeast-2" }),
+  REACT_APP_AWS_REGION: str({ default: 'ap-southeast-2' }),
   REACT_APP_COGNITO_IDENTITY_ID: str(),
   REACT_APP_COGNITO_USER_POOL_ID: str(),
   REACT_APP_COGNITO_WEB_CLIENT_ID: str(),
@@ -19,9 +17,20 @@ const awsAmplifyConfig = {
     identityPoolId: parsedEnv.REACT_APP_COGNITO_IDENTITY_ID,
     userPoolId: parsedEnv.REACT_APP_COGNITO_USER_POOL_ID,
     userPoolWebClientId: parsedEnv.REACT_APP_COGNITO_WEB_CLIENT_ID,
+    redirectSignIn: 'http://localhost:3000',
+    redirectSignOut: 'http://localhost:3000',
   },
 };
 configureAmplify(awsAmplifyConfig);
+Auth.configure({
+  oauth: {
+    domain: 'agiledigital-fedex-crm.auth.ap-southeast-2.amazoncognito.com',
+    scope: ['email', 'profile', 'openid'],
+    redirectSignIn: 'http://localhost:3000',
+    redirectSignOut: 'http://localhost:3000',
+    responseType: 'token',
+  },
+});
 // const AuthorisedApp = withAuthenticator(App, false);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
